4d293cf4f2b2a65d7dc1975a44301c2816630a90
Arimatea Neto <arineto30@gmail.com>
2015-10-05 11:40:26 -0300

    Adicionando botao de buscar mais campo de busca do endereco

--- app/src/main/java/com/arimateaneto/inloco/MapsActivity.java
+++ app/src/main/java/com/arimateaneto/inloco/MapsActivity.java
@@ -1,7 +1,11 @@
 package com.arimateaneto.inloco;
 
+import android.app.AlertDialog;
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -24,23 +28,35 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap = googleMap;
+        mMap.setMyLocationEnabled(true);
+
+        mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+            @Override
+            public void onMyLocationChange(Location location) {
+                LatLng myLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 13));
+            }
+        });
+    }
+
+    public void searchAddress(View view){
+        EditText address_text = (EditText)findViewById(R.id.address_text);
+        String address = address_text.getText().toString();
+
+        if(address != null && !address.equals("")) {
+            AlertDialog dialog = new AlertDialog.Builder(this).create();
+            dialog.setMessage(address);
+            dialog.show();
+        }
+    }
+
+    public void searchLocation(View view){
+        AlertDialog dialog = new AlertDialog.Builder(this).create();
+        dialog.setMessage("Vai buscar os locais");
+        dialog.show();
     }
 }
